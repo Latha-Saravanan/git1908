02236f11fa1d59decf61990d756bfdb17abde2a5
Latha-Saravanan <lathasaravanan1294@gmail.com>
2022-08-19 13:58:21 +0530

    Firstgit

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,33 @@
+package org.emp;
+
+public class Employee {
+	private void empId() {
+		System.out.println("Employee Id is L12");// TODO Auto-generated method stub
+	}
+	private void empName() {
+		System.out.println("Employee Name is Lauren");// TODO Auto-generated method stub
+
+	}private void empDob() {
+		System.out.println("Employee Dob is 12thMay1994");// TODO Auto-generated method stub
+
+	}private void empPhone() {
+		System.out.println("Employee Phone is 123456789");// TODO Auto-generated method stub
+
+	}private void empMail() {
+		System.out.println("Employee Mail is Lauren@gmail.com");// TODO Auto-generated method stub
+
+	}private void empAddress() {
+		System.out.println("Employee Address is 12/b, Vinayagar Street, Chennai-97");// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		Employee ED=new Employee();
+		ED.empId();
+		ED.empName();
+		ED.empDob();
+		ED.empPhone();
+		ED.empMail();
+		ED.empAddress();
+	}
+
+}
